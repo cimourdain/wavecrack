b1bcb44545fb78f5ad429ff6fdc14d9a3f49990a
cimourdain <gabriel.oger@gmail.com>
2018-10-11 19:53:14 +0200

    commenting

--- app/models/cracks/request.py
+++ app/models/cracks/request.py
@@ -6,7 +6,6 @@ import json
 
 # third party imports
 from sqlalchemy.orm import relationship
-from celery.result import AsyncResult
 
 # local imports
 from server import app, celery
@@ -14,11 +13,20 @@ from app import db
 from app.ref.hashes_list import HASHS_LIST
 from app.helpers.files import FilesHelper
 from app.models.cracks.entity import Crack
+# do not remove, used in request relationships
 from app.models.user import User
 from app.ref.close_modes import REQUESTS_CLOSE_MODES
 
 
 class CrackRequest(db.Model):
+    """
+    Model used to define Crack Request object.
+
+    A crack request is created by each crack form submission.
+    A crack request can trigger multiple cracks depending on form data submitted
+
+    * Stored in cracks_requests table
+    """
     __tablename__ = 'cracks_requests'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -44,15 +52,13 @@ class CrackRequest(db.Model):
     cracks = relationship("Crack", back_populates="request")
     user = relationship("User", back_populates="cracks_requests")
 
-    def init_request_folder(self):
-        self.session_id = str(uuid.uuid4())
-        if not FilesHelper.dir_exists(self.request_working_folder):
-            app.logger.debug("create directory "+str(self.request_working_folder))
-            os.mkdir(self.request_working_folder)
-        FilesHelper.file_exists(self.outfile_path, create=True)
-
+    # GETTERS : DYNAMIC PROPERTIES
     @property
     def hashes_type_code(self):
+        """
+        return _hashes_type_code hybrid property (set as hybrid because it require a setter to check its validity)
+        :return: <int>
+        """
         return self._hashes_type_code
 
     @property
@@ -61,11 +67,7 @@ class CrackRequest(db.Model):
             if int(h["code"]) == int(self.hashes_type_code):
                 return h["name"]
 
-    @hashes_type_code.setter
-    def hashes_type_code(self, code):
-        for h in HASHS_LIST:
-            if int(h["code"]) == int(code):
-                self._hashes_type_code = int(code)
+
 
     @property
     def request_working_folder(self):
@@ -98,6 +100,113 @@ class CrackRequest(db.Model):
         )
         return True if not FilesHelper.count_folders_in_dir(self.request_working_folder) else False
 
+    @property
+    def dictionary_paths(self):
+        if self._dictionary_paths:
+            for d in self._dictionary_paths.split(','):
+                d_folder, d_filename = FilesHelper.split_path(d, return_file_ext=False)
+                yield d_filename
+
+    @property
+    def keywords(self):
+        for d in self._dictionary_paths.split(','):
+            if not d.startswith(app.config["DIR_LOCATIONS"]["wordlists"]):
+                return FilesHelper.get_file_content(d)
+
+    @property
+    def mask(self):
+        return FilesHelper.get_file_content(self.mask_path)
+
+    @property
+    def has_mask(self):
+        if self.mask_path and FilesHelper.file_exists(self.mask_path):
+            return True
+        return False
+
+    @property
+    def rules(self):
+        for d in self.rules_path.split(','):
+            yield d
+
+    @property
+    def extra_options(self):
+        if self._extra_options:
+            app.logger.debug("extra options: "+str(self._extra_options))
+            return json.loads(self._extra_options)
+
+        return []
+
+    @property
+    def nb_password_found(self):
+        if self.use_potfile:
+            return FilesHelper.nb_lines_in_file(self.potfile_path)
+
+        if self.is_finished:
+            return FilesHelper.nb_lines_in_file(self.outfile_path)
+        else:
+            nb_passwords = 0
+            for c in self.cracks:
+                nb_passwords += c.nb_password_found
+            return nb_passwords
+
+    @property
+    def status(self):
+        if self.is_archived:
+            return "archived"
+
+        if not self.is_finished:
+            return "running"
+
+        if self.end_mode:
+            return REQUESTS_CLOSE_MODES[self.end_mode]
+
+        return "waiting"
+
+    @property
+    def cracks_progress(self):
+        if not self.cracks:
+            return 0
+
+        nb_finished_cracks = 0
+        for c in self.cracks:
+            if c.end_date:
+                nb_finished_cracks += 1
+
+        return round((float(nb_finished_cracks) / len(self.cracks)) * 100, 2)
+
+    @property
+    def password_progress(self):
+        if not self.nb_password_to_find:
+            return 0
+
+        return round((float(self.nb_password_found) / self.nb_password_to_find)*100, 2)
+
+    @property
+    def is_expired(self):
+        return (self.start_date + timedelta(days=self.duration)) <= datetime.now()
+
+    @property
+    def is_finished(self):
+        for c in self.cracks:
+            if not c.end_mode:
+                return False
+
+        self.end_mode = "ALL_PERFORMED"
+        db.session.commit()
+
+        return True
+
+    # SETTERS
+    @hashes_type_code.setter
+    def hashes_type_code(self, code):
+        """
+        setter of _hashes_type_code hybrid property.
+        Checks that hashes type code exists in reference HASH_LIST
+        """
+        for h in HASHS_LIST:
+            if int(h["code"]) == int(code):
+                self._hashes_type_code = int(code)
+
     @hashes.setter
     def hashes(self, hashes):
         # create hashes file (will be decremented of found passwords during process)
@@ -118,12 +227,45 @@ class CrackRequest(db.Model):
             os.path.join(self.request_working_folder, "hashes_original.txt")
         )
 
-    @property
-    def dictionary_paths(self):
-        if self._dictionary_paths:
-            for d in self._dictionary_paths.split(','):
-                d_folder, d_filename = FilesHelper.split_path(d, return_file_ext=False)
-                yield d_filename
+    @keywords.setter
+    def keywords(self, keywords):
+        keyword_file_path = FilesHelper.create_new_file(
+            file_path=self.request_working_folder,
+            file_name="keywords.txt",
+            content=keywords
+        )
+
+        self.add_dictionary_paths(keyword_file_path, ref=False)
+
+    @mask.setter
+    def mask(self, mask):
+        self.mask_path = FilesHelper.create_new_file(
+            file_path=self.request_working_folder,
+            file_name="mask.hcmask",
+            content=mask
+        )
+
+    @rules.setter
+    def rules(self, rules_paths):
+        if isinstance(rules_paths, basestring):
+            rules_paths = [rules_paths]
+
+        current_options = self.extra_options
+        for rp in rules_paths:
+            current_options.append({
+                "option": "--rules-file",
+                "value": os.path.join(app.config["DIR_LOCATIONS"]["rules"], str(rp))
+            })
+
+        self._extra_options = json.dumps(current_options, indent=4)
+
+    # METHODS
+    def init_request_folder(self):
+        self.session_id = str(uuid.uuid4())
+        if not FilesHelper.dir_exists(self.request_working_folder):
+            app.logger.debug("create directory "+str(self.request_working_folder))
+            os.mkdir(self.request_working_folder)
+        FilesHelper.file_exists(self.outfile_path, create=True)
 
     def add_dictionary_paths(self, list_of_wordlists, ref=False):
         app.logger.debug("request :: add_dictionary_paths :: add {} to list of dictionaries".format(
@@ -146,67 +288,6 @@ class CrackRequest(db.Model):
                 current_dict_path_list.append(os.path.join(prepath, d))
 
         self._dictionary_paths = ','.join(current_dict_path_list)
-
-    @property
-    def keywords(self):
-        for d in self._dictionary_paths.split(','):
-            if not d.startswith(app.config["DIR_LOCATIONS"]["wordlists"]):
-                return FilesHelper.get_file_content(d)
-
-    @keywords.setter
-    def keywords(self, keywords):
-        keyword_file_path = FilesHelper.create_new_file(
-            file_path=self.request_working_folder,
-            file_name="keywords.txt",
-            content=keywords
-        )
-
-        self.add_dictionary_paths(keyword_file_path, ref=False)
-
-    @property
-    def mask(self):
-        return FilesHelper.get_file_content(self.mask_path)
-
-    @property
-    def has_mask(self):
-        if self.mask_path and FilesHelper.file_exists(self.mask_path):
-            return True
-        return False
-
-    @mask.setter
-    def mask(self, mask):
-        self.mask_path = FilesHelper.create_new_file(
-            file_path=self.request_working_folder,
-            file_name="mask.hcmask",
-            content=mask
-        )
-
-    @property
-    def rules(self):
-        for d in self.rules_path.split(','):
-            yield d
-
-    @rules.setter
-    def rules(self, rules_paths):
-        if isinstance(rules_paths, basestring):
-            rules_paths = [rules_paths]
-
-        current_options = self.extra_options
-        for rp in rules_paths:
-            current_options.append({
-                "option": "--rules-file",
-                "value": os.path.join(app.config["DIR_LOCATIONS"]["rules"], str(rp))
-            })
-
-        self._extra_options = json.dumps(current_options, indent=4)
-
-    @property
-    def extra_options(self):
-        if self._extra_options:
-            app.logger.debug("extra options: "+str(self._extra_options))
-            return json.loads(self._extra_options)
-
-        return []
 
     def prepare_cracks(self):
         """
@@ -261,9 +342,6 @@ class CrackRequest(db.Model):
 
         db.session.commit()
 
-    def is_expired(self):
-        return (self.start_date + timedelta(days=self.duration)) <= datetime.now()
-
     def run_cracks(self):
         for c in self.cracks:
             c.run()
@@ -272,7 +350,7 @@ class CrackRequest(db.Model):
                 self.close_crack_request("ALL_FOUND")
                 break
 
-            if self.is_expired():
+            if self.is_expired:
                 self.close_crack_request("EXPIRED")
                 break
 
@@ -288,42 +366,6 @@ class CrackRequest(db.Model):
         self.end_date = datetime.now()
         db.session.commit()
 
-    @property
-    def nb_password_found(self):
-        if self.use_potfile:
-            return FilesHelper.nb_lines_in_file(self.potfile_path)
-
-        if self.is_finished():
-            return FilesHelper.nb_lines_in_file(self.outfile_path)
-        else:
-            nb_passwords = 0
-            for c in self.cracks:
-                nb_passwords += c.nb_password_found
-            return nb_passwords
-
-    @property
-    def status(self):
-        if self.is_archived:
-            return "archived"
-
-        if not self.is_finished():
-            return "running"
-
-        if self.end_mode:
-            return REQUESTS_CLOSE_MODES[self.end_mode]
-
-        return "waiting"
-
-    def is_finished(self):
-        for c in self.cracks:
-            if not c.end_mode:
-                return False
-
-        self.end_mode = "ALL_PERFORMED"
-        db.session.commit()
-
-        return True
-
     def force_close(self):
         # close celery task
         celery.control.revoke(self.celery_request_id)
@@ -333,22 +375,3 @@ class CrackRequest(db.Model):
             crack.force_close()
 
         self.close_crack_request(mode="MANUAL")
-
-    @property
-    def cracks_progress(self):
-        if not self.cracks:
-            return 0
-
-        nb_finished_cracks = 0
-        for c in self.cracks:
-            if c.end_date:
-                nb_finished_cracks += 1
-
-        return round((float(nb_finished_cracks) / len(self.cracks)) * 100, 2)
-
-    @property
-    def password_progress(self):
-        if not self.nb_password_to_find:
-            return 0
-
-        return round((float(self.nb_password_found) / self.nb_password_to_find)*100, 2)

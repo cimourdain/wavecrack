39b4b63d4f894d3a493f978ac76a85a89405652d
cimourdain <gabriel.oger@gmail.com>
2018-10-19 17:17:11 +0200

    init unit testing 2

---
+++ tests/pytest_init.py
@@ -0,0 +1,36 @@
+import pytest
+
+from app import create_app, db, register_routes
+
+
+@pytest.fixture
+def client():
+    # build app
+    app = create_app('testing')
+    register_routes(app)
+
+    # enter context
+    app_ctx = app.app_context()
+    app_ctx.push()
+
+    # reset and init database
+    from app.models.user import User
+    from app.models.cracks.entity import Crack
+    from app.models.cracks.request import CrackRequest
+
+    db.reflect()
+    db.drop_all()
+
+    db.create_all()
+    User.insert_default_users()
+
+    # setup client
+    client = app.test_client(use_cookies=True)
+
+    yield client
+
+    db.session.remove()
+    app_ctx.pop()
+
+
+
